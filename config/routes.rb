e2938bb8d58bd4593a16bef7c5b0602dc72ac6f2
David Clark <david.clark1@kellogg.northwestern.edu>
2025-06-05 23:24:18 +0000

    Define routes using hash-rocket style matching starter-app-with-auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "places", :action => "index" })
-  resources "entries"
-  resources "places"
-  resources "sessions"
-  resources "users"
+  # User signup
+  get("/signup", { :controller => "users",    :action => "new"   })
+  post("/signup", { :controller => "users",    :action => "create" })
+
+  # Login / Logout
+  get("/login",  { :controller => "sessions", :action => "new"     })
+  post("/login", { :controller => "sessions", :action => "create"  })
+  get("/logout", { :controller => "sessions", :action => "destroy" })
+
+  # Nested resources: Places and Entries
+  resources "places" do
+    resources "entries", { :only => ["new", "create"] }
+  end
+
+  # Root route
+  get("/", { :controller => "sessions", :action => "new" })
 end
