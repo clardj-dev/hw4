e032060aa2224bc4dc803de87dd99e4323be2407
David Clark <david.clark1@kellogg.northwestern.edu>
2025-06-05 23:10:59 +0000

    Populate User model with has_secure_password, associations, validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  # A User "has many" Places. Deleting a user destroys their places.
+  has_many("places", "dependent" => :destroy)
+
+  validates("username", "presence" => true)
+  validates("email",    "presence" => true, "uniqueness" => true)
 end
