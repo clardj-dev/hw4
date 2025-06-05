46322b16912ad9552b9947dc1b9672f0755a60ab
David Clark <david.clark1@kellogg.northwestern.edu>
2025-06-05 23:56:11 +0000

    Scope PlacesController to current_user, add require_login and set_place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,22 +1,40 @@
 class PlacesController < ApplicationController
+before_action :require_login
+before_action :set_place, only: ["show"]
 
   def index
-    @places = Place.all
+    @places = current_user.places.order({ "created_at" => :desc })
   end
 
   def show
-    @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    @entries = @place.entries.order({ "occurred_on" => :desc })
   end
 
   def new
+    @place = current_user.places.new
   end
 
   def create
-    @place = Place.new
-    @place["name"] = params["name"]
-    @place.save
-    redirect_to "/places"
-  end
+    @place = current_user.places.new
+    @place["name"]        = params["place"]["name"]
+    @place["location"]    = params["place"]["location"]
+    @place["description"] = params["place"]["description"]
 
+    if @place.save
+      flash["notice"] = "Place created!"
+      redirect_to "/places/#{@place["id"]}"
+    else
+      render :new
+  end
 end
+
+  private
+
+  # Load the current_user's place (so nobody can see another user's)
+  def set_place
+    @place = current_user.places.find_by({ "id" => params["id"] })
+    if @place == nil
+      flash["notice"] = "Not authorized to view that place"
+      redirect_to "/places"
+    end
+  end
