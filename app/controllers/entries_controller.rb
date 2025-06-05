a6a3050719cc69b514a6133b6780dd86f25b75ad
David Clark <david.clark1@kellogg.northwestern.edu>
2025-06-05 23:58:48 +0000

    Scope EntriesController under current_user, add require_login and set_place, handle image

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,16 +1,34 @@
 class EntriesController < ApplicationController
+before_action :require_login
+before_action :set_place
 
   def new
+    @entry = @place.entries.new
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    @entry = @place.entries.new
+    @entry["title"]       = params["entry"]["title"]
+    @entry["long_desc"]   = params["entry"]["long_desc"]
+    @entry["occurred_on"] = params["entry"]["occurred_on"]
+    @entry["image"]       = params["entry"]["image"]
+
+    if @entry.save
+      flash["notice"] = "Entry added!"
+      redirect_to "/places/#{@place["id"]}"
+    else
+      render :new
+    end
   end
+
+  private
+
+  def set_place
+    @place = current_user.places.find_by({ "id" => params["place_id"] })
+    if @place == nil
+      flash["notice"] = "Not authorized to add an entry"
+      redirect_to "/places"
+    end
+  end  
 
 end
